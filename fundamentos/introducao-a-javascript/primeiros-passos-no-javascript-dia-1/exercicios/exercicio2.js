58cdcdf09bce35ce21cce6b58b436ae3d7e6660a
Beatriz de Freitas <beatriz.freitas.almeida@hotmail.com>
2023-05-31 18:54:37 -0300

    added exercise2

---
+++ fundamentos/introducao-a-javascript/primeiros-passos-no-javascript-dia-1/exercicios/exercicio2.js
@@ -0,0 +1,12 @@
+let numero1 = 14;
+let numero2 = 55;
+message = '';
+
+if (numero1 > numero2){
+message = numero1 + " eh maior que " + numero2;}
+else if(numero2 > numero1){
+message = numero2 + " eh maior que " + numero1;}
+else {
+message = numero1 + " eh igual a " + numero2;
+}
+console.log(message)
