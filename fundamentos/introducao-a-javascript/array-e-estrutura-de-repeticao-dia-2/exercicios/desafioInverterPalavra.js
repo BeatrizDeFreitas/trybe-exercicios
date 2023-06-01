a4ee3f51f2a2c3556ed1c62879ebb811933e79fd
Beatriz de Freitas <beatriz.freitas.almeida@hotmail.com>
2023-06-01 16:14:14 -0300

    finished

---
+++ fundamentos/introducao-a-javascript/array-e-estrutura-de-repeticao-dia-2/exercicios/desafioInverterPalavra.js
@@ -0,0 +1,14 @@
+let word = 'tryber';
+let reverseWord = '';
+
+for (let index = word.length - 1; index >= 0; index -= 1) {
+  reverseWord += word[index];
+};
+
+console.log(reverseWord);
+
+// ou
+
+reverseWord = word.split('').reverse().join('');
+
+console.log(reverseWord);
